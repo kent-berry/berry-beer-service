03529a2ef0afdb1665176b460cb258728231e15f
Kent Berry <kentwberry@protonmail.com>
2023-03-03 13:58:02 -0500

    Spring Microservice practice
    initial project add

---
+++ src/main/java/com/berrybeerservice/BerryBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.berrybeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerryBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BerryBeerServiceApplication.class, args);
+    }
+
+}
