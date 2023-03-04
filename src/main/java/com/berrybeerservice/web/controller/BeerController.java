89e9e467c34d684714b54c043f6e33b59c2fea6d
Kent Berry <kentwberry@protonmail.com>
2023-03-04 16:18:00 -0500

    adding models, controllers, jpa entities, spring data repos, bootstrap data

---
+++ src/main/java/com/berrybeerservice/web/controller/BeerController.java
@@ -0,0 +1,37 @@
+package com.berrybeerservice.web.controller;
+
+import com.berrybeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping(BeerController.API_V_1_BEER)
+@RestController
+public class BeerController {
+
+    public static final String API_V_1_BEER = "/api/v1/beer";
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+
+        //todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+
+        //todo impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+
+        //todo impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+}
